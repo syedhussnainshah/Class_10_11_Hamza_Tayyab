5f1a55861d0d9635df5cb376f2a851f53de3c51a
Syed Hussnain Shah <itshussnain787@gmail.com>
2022-07-19 11:46:17 +0500

    update

---
+++ 19-07-22/index.js
@@ -0,0 +1,20 @@
+var a = 5;
+var b = 4;
+
+if (a < b) {
+  console.log("Yesy Your are Right");
+} else {
+  console.log("Sorry! Your Are Wrong");
+}
+
+// b = 3;
+// if(a==b){
+//     console.log('OK True');
+// }else{
+//     console.log("Not True");
+// }
+// if (a === b) {
+//     console.log("Yes true ");
+// } else {
+//     console.log("Your Condition is not True");
+// }
