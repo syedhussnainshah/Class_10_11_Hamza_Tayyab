0de0bab8e2f84c7d47e4769bb0b00543db11b5e8
Syed Hussnain Shah <itshussnain787@gmail.com>
2022-07-20 10:47:41 +0500

    update

---
+++ 20-07-22/index.js
@@ -0,0 +1,16 @@
+var a = prompt("PLease Enter Obtain");
+var b = prompt("ENter your Total Number");
+a = parseInt(a);
+if (a <= 10) {
+  console.log("Your Too much Fail");
+} else if (a <= 20) {
+  console.log("you are also Fail");
+} else if (a <= 30) {
+  console.log("You are Near to pass");
+} else if (a <= 40) {
+  console.log("congratulation You are Very difficult to pass");
+} else {
+  if (a <= 50) {
+    console.log("Congratulation You are Inter In Pakistan");
+  }
+}
