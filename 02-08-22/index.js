8eaa41b02444b9d6b5eb15800d7c5bec6df7ec51
Syed Hussnain Shah <itshussnain787@gmail.com>
2022-08-02 09:41:53 +0500

    update

---
+++ 02-08-22/index.js
@@ -0,0 +1,33 @@
+let btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+  let input = document.getElementById("todoinput");
+  let ul = document.getElementById("unorder");
+  let li = document.createElement("li");
+  let inputTodo = document.createElement("input");
+  inputTodo.setAttribute("type", "text");
+  inputTodo.setAttribute("value", input.value);
+  inputTodo.setAttribute("readonly", "readonly");
+  let edit = document.createElement("button");
+  edit.innerHTML = "Edit";
+  edit.setAttribute("class", "btn btn-primary");
+  let del = document.createElement("button");
+  del.innerHTML="Delete";
+  del.setAttribute("class", "btn btn-danger");
+
+  edit.addEventListener("click", function () {
+    if (edit.innerHTML == "Edit") {
+      edit.innerHTML = "Save";
+      inputTodo.removeAttribute("readonly", "readonly");
+    } else {
+      inputTodo.setAttribute("readonly", "readonly");
+      edit.innerHTML = "Edit";
+    }
+  });
+
+  li.appendChild(inputTodo);
+  li.appendChild(edit);
+  li.appendChild(del);
+  ul.appendChild(li);
+
+  console.log(inputTodo);
+});
