fb1c4f2979ac3b748bb3a0a2f9d4344dd8e56a9d
Syed Hussnain Shah <itshussnain787@gmail.com>
2022-08-12 10:54:12 +0500

    update

---
+++ 12-08-22/index.js
@@ -0,0 +1,14 @@
+let array = ["HTML", "CSS", "BOOTSTRAP", "JAVASCRIPT", "PHP"];
+console.log(array.length);
+
+// array.sort();
+// array.reverse();
+// array.shift();
+// array.unshift("TEST","TEST@");
+// array.push("YES");
+// array.pop();
+// array=array.slice(1,3);
+// array=array.splice(1,1,"TEST");
+
+console.log(array);
+
